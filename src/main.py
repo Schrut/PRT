264f3b8d8116a60c0d32672d174994de448b09a7
spokendotcpp <thibault.heckel@protonmail.com>
2018-02-12 01:27:19 +0100

    Add .gitignore, License + first UI compo

--- src/main.py
+++ src/main.py
@@ -1,80 +1,16 @@
-"""
-Main module
-
-Tout ce qui est dans ces guillemets s'appelle une docstring.
-
-Pour VisualStudioCode:
-installer `pylint`:
-    # pip install pylint
-    (le # ici c'est pour sudo (( enfin à faire en admin )), une convention, et oui une de plus, sinon ce serait $)
-"""
-#!/usr/bin/python3
-
-#
-# Un très bon tuto qui permet de prendre
-# en main très facilement la lib pyQt5
-# http://zetcode.com/gui/pyqt5/firstprograms/
-#
-
-
 import sys
 
 from PyQt5.QtWidgets import (
     QApplication,
-    QWidget,
-    QPushButton,
 )
 
-# Ici la classe NewWindow hérite de QWidget
-# C'est-à-dire qu'elle pourra utiliser toutes
-# les fonctions de la classe QWidget
-# Par exemple, `.show()`
-class NewWindow(QWidget):
-    """
-    A New Window class
-    """
-
-    # Un commentaire python
-    # 
-    # __init__ ici représente le constructeur de la classe
-    # `NewWindow`
-    # Il sera appelé automatiquement quand on appelera la fonction
-    # `NewWindow()` qui créera donc une nouvelle instance
-    # de NewWindow.
-    #
-    # le mot clé self, fait bien évidement référence
-    # à l'instance de la classe elle même.
-    # On cherche à construire ici un objet.
-    #
-    # On remarque d'ailleurs que le constructeur ne prend pas d'arguments
-    def __init__(self):
-        super().__init__()
-        self.build()
-
-    def build(self):
-        """
-        init UI of NewWindow
-        """
-        # Création d'un bouton qui permet de quitter l'application
-        btn = QPushButton("Button", self)
-        # On "connect" notre boutton à une action lorsqu'il est dans l'état "clicked"
-        btn.clicked.connect(QApplication.instance().quit)
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
-
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle("Main test")
-        self.show()
-
+from ui import uiMainWindow
 
 def main():
-    """
-    Main function
-    """
     app = QApplication(sys.argv)
-    win = NewWindow()
+    win = uiMainWindow()
+    win.show()
     sys.exit(app.exec_())
-    #ici on utilse exec_ avec un "underscore" car "exec" est déjà un mot clé du langage Python
 
 if __name__ == "__main__":
     main()
---
+++ src/ui.py
@@ -0,0 +1,89 @@
+from PyQt5.QtWidgets import (
+    QMainWindow,
+    QMessageBox,
+    QWidget,
+    QMenuBar,
+    QAction,
+    QWidget,
+    QDialog,
+    QLabel,
+    QMenu,
+)
+
+from PyQt5.QtCore import pyqtSlot
+
+"""
+The MIT License (MIT)
+https://mit-license.org/
+"""
+class uiLicenseWindow(QMessageBox):
+    def __init__(self, window):
+        super().__init__(window)
+        self.title = "The MIT License (MIT)"
+        self.license = "<pre><b>Copyright © 2018  <i>~ Thibault HECKEL, Florian GIMENEZ ~</i></b><br><br>\
+Permission is hereby granted, free of charge, to any person obtaining a copy <br>\
+of this software and associated documentation files (the “Software”), <br>\
+to deal in the Software without restriction, including without limitation <br>\
+the rights to use, copy, modify, merge, publish, distribute, sublicense, <br>\
+and/or sell copies of the Software, and to permit persons to whom the Software <br>\
+is furnished to do so, subject to the following conditions:<br><br>\
+The above copyright notice and this permission notice shall be included in <br>\
+all copies or substantial portions of the Software.<br><br>\
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, <br>\
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES <br>\
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. <br>\
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, <br>\
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, <br>\
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE <br>\
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</pre><br>\
+Read more at: <a href=\"https://opensource.org/licenses/MIT\">https://opensource.org/licenses/MIT</a>"
+
+        self.window = window
+    
+    def on_clik(self):
+        self.license.center
+        self.information(self.window, self.title, self.license, QMessageBox.Ok)
+
+"""
+User Interface Main Window
+"""
+class uiMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = "PRT"
+        self.left = 200
+        self.top = 200
+        self.width = 1080
+        self.height = 720
+        self.build()
+
+    def btn_click(self):
+            QMessageBox.information(self, "test", "message test", QMessageBox.Ok, QMessageBox.Ok)
+
+    def build(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        # Buttons
+        ## Exit button
+        button_exit = QAction("Exit", self)
+        button_exit.setShortcut("Ctrl+Q")
+        button_exit.setStatusTip("Exit application")
+        button_exit.triggered.connect(self.close)
+
+        ## Show License button
+        button_license = QAction("License", self)
+        button_license.setStatusTip("Application's license")
+        button_license.triggered.connect(uiLicenseWindow(self).on_clik)
+
+        # Menus
+        # Main menu
+        menu = self.menuBar()
+
+        ## File menu
+        menu_file = menu.addMenu("File")
+        menu_file.addAction(button_exit)
+
+        ## About menu
+        menu_about = menu.addMenu("About")
+        menu_about.addAction(button_license)
