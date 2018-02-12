2f0a07f3cba17112b8bd3aecb975fec9d64de3e7
spokendotcpp <thibault.heckel@protonmail.com>
2018-02-12 01:44:46 +0100

    Fix License typo

--- src/ui.py
+++ src/ui.py
@@ -20,21 +20,21 @@ class uiLicenseWindow(QMessageBox):
         self.title = "The MIT License (MIT)"
         #Hardcoded License
         self.license = "<pre><b>Copyright © 2018  <i>~ Thibault HECKEL, Florian GIMENEZ ~</i></b><br><br>\
-Permission is hereby granted, free of charge, to any person obtaining a copy <br>\
-of this software and associated documentation files (the “Software”), <br>\
-to deal in the Software without restriction, including without limitation <br>\
-the rights to use, copy, modify, merge, publish, distribute, sublicense, <br>\
-and/or sell copies of the Software, and to permit persons to whom the Software <br>\
-is furnished to do so, subject to the following conditions:<br><br>\
-The above copyright notice and this permission notice shall be included in <br>\
-all copies or substantial portions of the Software.<br><br>\
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, <br>\
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES <br>\
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. <br>\
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, <br>\
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, <br>\
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE <br>\
-OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</pre><br>\
+Permission is hereby granted, free of charge, to any person obtaining a copy<br>\
+of this software and associated documentation files (the “Software”), to deal<br>\
+in the Software without restriction, including without limitation the rights<br>\
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell<br>\
+copies of the Software, and to permit persons to whom the Software is<br>\
+furnished to do so, subject to the following conditions:<br><br>\
+The above copyright notice and this permission notice shall be included in all<br>\
+copies or substantial portions of the Software.<br><br>\
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br>\
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,<br>\
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE<br>\
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER<br>\
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br>\
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE<br>\
+SOFTWARE.</pre><br>\
 Read more at: <a href=\"https://opensource.org/licenses/MIT\">https://opensource.org/licenses/MIT</a>"
     
     def on_clik(self):
