669ed8561f3e9b00f81112a940c8b18a30723e15
Schrut <gimflorian@gmail.com>
2018-03-02 10:56:31 +0100

    first try gdal #2

--- src/gdal_test.py
+++ src/gdal_test.py
@@ -1,18 +1,27 @@
 #gdal_translate - srcwin 0, 0, 958, 570 - a_srs "+proj=geos +a=6378169.0 +b=6356583.8 +lon_0=9.5 +h=35785831.0 +x_0=0 +y_0=0 +pm=0" - a_ullr - 1025637.42, 4614118.21, -67509.04, 4044041.83 20170407054917_MSG2.tif france_geos.tif
 from osgeo import gdal
 import numpy
-ds = gdal.Open('../tif/france_mercator.tif')
-ts = gdal.Open('../tif/20170407054917_MSG2.tif', gdal.GA_Update)
-
-geotransform = ds.GetGeoTransform()
-if geotransform:
-    print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
-    print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
-
-geotransformbis = ([-6.57105413966808, 0.007945883172990461, 0, 51.585583486581285, 0, -0.00838910302422845])
 
 
-ts = ts.SetGeoTransform(geotransformbis)
+def array_to_raster(array):
+    """Array > Raster
+    Save a raster from a C order array.
+
+    :param array: ndarray
+    """
+    ds = gdal.Open('../tif/france_mercator.tif')
+    ts = gdal.Open('../tif/20170407054917_MSG2.tif', gdal.GA_Update)
+
+    geotransform = ds.GetGeoTransform()
+    if geotransform:
+        print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
+        print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
+
+
+
+    ts = ts.SetGeoTransform(geotransform)
+    ts.SetProjection('+proj=geos +a=6378169.0 +b=6356583.8 +lon_0=9.5 +h=35785831.0 +x_0=0 +y_0=0 +pm=0')
+    ts.GetRasterBand(1).WriteArray(array)
 
 
 
