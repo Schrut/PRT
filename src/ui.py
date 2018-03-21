4b7dbb0aab631d9b690ad79a1140505985169064
Schrut <gimflorian@gmail.com>
2018-03-21 11:29:12 +0100

    Done bis

--- src/ui.py
+++ src/ui.py
@@ -94,6 +94,11 @@ class uiGdal(QMainWindow):
 
 	def handleButton_WG(self):
 		pathname_in = self.parent.cimg.pname
+		if not pathname_in.endswith('translate.tif'):
+			print('Not warp to do!')
+			print('Translate -> Warp')
+			self.close()
+			return False
 		pathname_out = os.path.basename(pathname_in)
 		pathname_out = '../GDAL/Warp/' + pathname_out[:-4] + '_mercator_warp.tif'
 		if os.path.exists('../GDAL/Warp/') is False:
@@ -106,7 +111,7 @@ class uiGdal(QMainWindow):
 	def handleButton_TG(self):
 		pathname_in = self.parent.cimg.pname
 		pathname_out = os.path.basename(pathname_in)
-		pathname_out = '../GDAL/Translate/' + pathname_out[:-4] + '_geos_traslate.tif'
+		pathname_out = '../GDAL/Translate/' + pathname_out[:-4] + '_geos_translate.tif'
 		if os.path.exists('../GDAL/Translate/') is False:
 			os.makedirs('../GDAL/Translate')
 		os.system('gdal_translate -srcwin 0, 0, 958, 570 -a_srs "+proj=geos +a=6378169.0 +b=6356583.8 +lon_0=9.5 +h=35785831.0 +x_0=0 +y_0=0 +pm=0" -a_ullr -1025637.42, 4614118.21, -67509.04, 4044041.83 ' + pathname_in + ' ' + pathname_out)
